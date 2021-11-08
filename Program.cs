35ab8a96bc231e595ae5ea7785cd6730b1557ca2
Ivo286 <93599972+Ivo286@users.noreply.github.com>
2021-11-08 15:48:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namirane_na_chetno_i_nechetno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Четно");
+            }
+            else
+            {
+                Console.WriteLine("Нечетно");
+            }
+            Console.ReadKey();
+        }
+    }
+}
